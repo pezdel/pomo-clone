d9d84fbb68acfc6fabbf3b9de360c94bf82e9cd7
pezdel <adam.pezdel@gmail.com>
2022-11-03 16:01:06 -0500

    sdf

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
    return (
       <div className={theme}>
          <div className="flex flex-col w-screen h-screen bg-primary text-white items-center transition ease-in-out delay-150">
-            <div className="flex flex-col w-full h-full items-center">
+            <div className="flex flex-col w-full h-full items-center bg-primary">
                <div className="flex pt-3 pb-10">
                   <Navbar />
                </div>
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
                   <TimerHeader />
                   <Timer />
                </div>
-               <div className='w-128 bg-primary'>
+               <div className='w-128'>
                   <TimerText />
                   <TaskList />
                </div>
