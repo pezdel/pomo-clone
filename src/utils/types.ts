454d34f45e2bb9f3a59d7c1bab9ef35e06b95cda
pezdel <adam.pezdel@gmail.com>
2022-11-03 14:37:01 -0500

    fixed types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -13,3 +13,21 @@ export interface SettingsColors {
    blue: string;
 }
 
+
+
+export interface TimeItem {
+   min: number;
+   sec: number;
+}
+
+export interface TaskItem {
+   id: number;
+   name: string;
+   time: {current: TimeItem, total: TimeItem};
+   count: {current: number, total: number};
+   complete: boolean;
+   fresh: boolean;
+}
+
+
+
